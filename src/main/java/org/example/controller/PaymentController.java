8d7e848b1995706c7491909cda09430079f87758
haripriya <rm.haripriya@gmail.com>
2023-12-18 21:25:31 +0530

    Intial Version 2

--- src/main/java/org/example/controller/PaymentController.java
+++ src/main/java/org/example/controller/PaymentController.java
@@ -3,12 +3,25 @@ package org.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PaymentController {
 
     @GetMapping(value = {"/", "/index"})
-    public String index(Model model) {
+    public String index(final Model model) {
+        model.addAttribute("title", "My Title");
+        return "index";
+    }
+
+    @GetMapping(value = "/pay")
+    public String pay(final Model model) {
+        model.addAttribute("title", "My Title");
+        return "index";
+    }
+
+    @PostMapping(value = "/pay-return-url")
+    public String paymentNotification(final Model model) {
         model.addAttribute("title", "My Title");
         return "index";
     }
