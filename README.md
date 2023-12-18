0c62ae853eb2947bf7d52a9cbdad32c95b1661ff
haripriya <rm.haripriya@gmail.com>
2023-12-18 21:56:42 +0530

    One Step

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # springboot-phonepe
 Spring Boot PhonePe Example
+
+## REF
+1. https://github.com/Code-180/PhonePe-Nodejs-Integration
+2. https://www.baeldung.com/spring-redirect-and-forward
--- src/main/java/org/example/controller/PaymentController.java
+++ src/main/java/org/example/controller/PaymentController.java
@@ -1,9 +1,17 @@
 package org.example.controller;
 
+import com.phonepe.sdk.pg.Env;
+import com.phonepe.sdk.pg.common.http.PhonePeResponse;
+import com.phonepe.sdk.pg.payments.v1.PhonePePaymentClient;
+import com.phonepe.sdk.pg.payments.v1.models.request.PgPayRequest;
+import com.phonepe.sdk.pg.payments.v1.models.response.PayPageInstrumentResponse;
+import com.phonepe.sdk.pg.payments.v1.models.response.PgPayResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.UUID;
 
 @Controller
 public class PaymentController {
@@ -16,7 +24,30 @@ public class PaymentController {
 
     @GetMapping(value = "/pay")
     public String pay(final Model model) {
-        model.addAttribute("title", "My Title");
+        String merchantId = "PGTESTPAYUAT";
+        String saltKey = "099eb0cd-02cf-4e2a-8aca-3e6c6aff0399";
+        Integer saltIndex = 1;
+        Env env = Env.UAT;
+        boolean shouldPublishEvents = true;
+        PhonePePaymentClient phonepeClient = new PhonePePaymentClient(merchantId, saltKey, saltIndex, env, shouldPublishEvents);
+
+        String merchantTransactionId = UUID.randomUUID().toString().substring(0,34);
+        long amount = 100;
+        String callbackurl = "http://localhost:8080/pay-return-url";
+        String merchantUserId = "MUID123";
+
+        PgPayRequest pgPayRequest = PgPayRequest.PayPagePayRequestBuilder()
+                .amount(amount)
+                .merchantId(merchantId)
+                .merchantTransactionId(merchantTransactionId)
+                .callbackUrl(callbackurl)
+                .merchantUserId(merchantUserId)
+                .build();
+
+        PhonePeResponse<PgPayResponse> payResponse = phonepeClient.pay(pgPayRequest);
+        PayPageInstrumentResponse payPageInstrumentResponse = (PayPageInstrumentResponse) payResponse.getData().getInstrumentResponse();
+        String url = payPageInstrumentResponse.getRedirectInfo().getUrl();
+        model.addAttribute("title", url);
         return "index";
     }
 
