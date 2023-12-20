5cbfbdc64ee49471c6aed203e9810a6a85d14ea7
Manikanta <manikantayarramsetti445@gmil.com>
2023-12-20 18:57:45 +0530

    checkstyle fixing

--- src/main/java/org/example/config/PhonePeProperties.java
+++ src/main/java/org/example/config/PhonePeProperties.java
@@ -5,9 +5,15 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
  * The type App properties.
+ * @param merchantId
+ * @param saltKey
+ * @param saltIndex
+ * @param env
+ * @param shouldPublishEvents
+ * @param callbackUrl
  */
 @ConfigurationProperties(prefix = "spring.payments.phonepe")
-public record PhonePeProperties (String merchantId,
+public record PhonePeProperties(String merchantId,
                              String saltKey,
                              Integer saltIndex,
                                  Env env, boolean shouldPublishEvents,
